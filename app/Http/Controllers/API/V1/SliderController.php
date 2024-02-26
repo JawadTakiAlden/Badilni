dc89fbd6d94af7f3d57ddd1604fa6072defaf294
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-02-26 13:57:37 -0500

    create slider controller and home controller

--- app/Http/Controllers/API/V1/SliderController.php
+++ app/Http/Controllers/API/V1/SliderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\HelperMethods\HelperMethod;
+use App\Http\Controllers\Controller;
 use App\Http\Requests\API\V1\Slider\CreateSlideRequest;
 use App\Http\Requests\API\V1\Slider\UpdateSlideRequest;
 use App\Http\Resources\SliderResource;
