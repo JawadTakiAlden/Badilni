62a581a1fc6963a477d1f3270d60b15621c81dfc
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-02-27 16:48:19 -0500

    add some isuues

--- app/Http/Resources/API/V1/AreaResource.php
+++ app/Http/Resources/API/V1/AreaResource.php
@@ -18,7 +18,7 @@ class AreaResource extends JsonResource
         return [
             'id' =>$this->id,
             'title' => HelperMethod::extractValueDependOnLanguageOfRequestUser($this->title),
-            'is_active' => $this->is_active,
+            'is_active' => boolval($this->is_active),
             'city' => HelperMethod::extractValueDependOnLanguageOfRequestUser($this->city->title),
         ];
     }
