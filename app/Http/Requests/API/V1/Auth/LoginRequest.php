689840d9fb28b55fc7f4a3ca53ba6849cf111791
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-03-01 12:43:57 -0500

    some updates

--- app/Http/Requests/API/V1/Auth/LoginRequest.php
+++ app/Http/Requests/API/V1/Auth/LoginRequest.php
@@ -23,7 +23,7 @@ class LoginRequest extends FormRequest
     {
         return [
             'email' => 'required|email',
-            'password' => 'required|min:6',
+            'password' => 'required|min:4',
             'device_type'        => 'required|in:ios,android,web',
             'device_uuid'        => 'required',
             'notification_token' => 'required'
