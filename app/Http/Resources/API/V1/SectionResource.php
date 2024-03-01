6f82f495e576ffc87f035471e595070a9ee2489e
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-03-01 12:31:54 -0500

    some updates

--- app/Http/Resources/API/V1/SectionResource.php
+++ app/Http/Resources/API/V1/SectionResource.php
@@ -18,7 +18,7 @@ class SectionResource extends JsonResource
         return [
           'id' => $this->id,
           'title' => HelperMethod::extractValueDependOnLanguageOfRequestUser($this->title),
-          'is_active' => $this->is_active
+          'is_active' => boolval($this->is_active)
         ];
     }
 }
