4c27a1813e667c89339aacb47c5711e41eebe05b
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-03-23 09:04:58 -0400

    fix home

--- app/Http/Controllers/API/V1/HomeController.php
+++ app/Http/Controllers/API/V1/HomeController.php
@@ -29,10 +29,15 @@ class HomeController extends Controller
             $sections = Section::where('is_active' , true)->get();
             $categories = Category::where('is_active' , true)->orderBy('sort' , 'desc')->get();
             $sliders = Slider::where('is_active' , true)->where('type'  , 'home')->orderBy('sort' , 'desc')->get();
+//            return $this->success([
+//               'sections' => SectionResource::collection($sections),
+//               'categories' => CategoryResource::collection($categories),
+//               'sliders' => SliderResource::collection($sliders)
+//            ]);
             return $this->success([
-               'sections' => SectionResource::collection($sections),
-               'categories' => CategoryResource::collection($categories),
-               'sliders' => SliderResource::collection($sliders)
+                'sections' =>$sections,
+                'categories' =>$categories,
+                'sliders' => $sliders,
             ]);
         }catch (\Throwable $th){
             return $this->helper->getErrorResponse($th);
