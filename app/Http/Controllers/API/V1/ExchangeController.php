ab4a5dfa9effc96ab1d8fd41dffcf7ed6b81008f
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2024-04-06 08:29:22 -0400

    check exchange api

--- app/Http/Controllers/API/V1/ExchangeController.php
+++ app/Http/Controllers/API/V1/ExchangeController.php
@@ -99,7 +99,7 @@ class ExchangeController extends Controller
         }catch (\Throwable $throwable){
             DB::rollBack();
 //            return $this->serverError();
-            return $this->serverError();
+            return $throwable->getMessage();
         }
     }
 
