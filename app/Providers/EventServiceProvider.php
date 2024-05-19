a7c05d7faa02046587f1888e1204eaf9eed4c9ec
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-05-19 11:25:29 +0300

    fix listener

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Events\SendMessageEvent;
-use App\Listeners\SendMessageListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
