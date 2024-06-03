c89742fdaefd07412aa87a9cf8399f7f42e34738
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-06-03 13:31:35 +0300

    phone , notification , accepted exchange update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('image')->nullable();
-            $table->string('phone');
             $table->enum("language",["en","ar"])->default("en");
             $table->dateTime('birthdate')->nullable();
             $table->enum('gender' , ['male' , 'female'])->nullable();
